b9b8e568515f1da1a326e8e05b3febed7f36c884
Abid <145914507+AbidAbdi@users.noreply.github.com>
2025-09-01 19:08:44 +0800

    rename gambar image2

--- resources/views/Layanan1.blade.php
+++ resources/views/Layanan1.blade.php
@@ -856,7 +856,7 @@
                     <!-- Left Content - House Image -->
                     <div class="space-y-8">
                         <div class="aspect-[4/5] bg-gray-800 rounded-lg overflow-hidden shadow-lg">
-                            <img src="{{ asset('img/project unggulan.jpg') }}"
+                            <img src="{{ asset('img/image2.jpg') }}"
                                 alt="Modern House Exterior" class="w-full h-full object-cover">
                         </div>
                     </div>
