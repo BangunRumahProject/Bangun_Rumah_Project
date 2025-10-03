1090803d99f33de31abb9ae6943aa93f37bb255f
Abid <145914507+AbidAbdi@users.noreply.github.com>
2025-10-03 17:53:22 +0800

    perbaikan logo/icon web 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,8 +15,12 @@
     <meta name="geo.position" content="-3.9985;122.5136">
     <meta name="ICBM" content="-3.9985, 122.5136">
     <title>@yield('title')</title>
-    <link rel="icon" type="image/png" href="{{ asset('public/img/LogoBR.png') }}?v=2">
-    <link rel="apple-touch-icon" href="{{ asset('public/img/LogoBR.png') }}?v=2">
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}?v=3">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/LogoBR.png') }}?v=3">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/LogoBR.png') }}?v=3">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/LogoBR.png') }}?v=3">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}?v=3">
     <!-- Font Awesome CDN for icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
@@ -31,7 +35,7 @@
     <meta property="og:title" content="@yield('title')">
     <meta property="og:description" content="@yield('meta_description', 'Bangun Rumah Project - kontraktor kendari, bangun rumah kendari, renovasi rumah kendari, desain arsitektur kendari, RAB & PBG/IMB kendari.')">
     <meta property="og:url" content="{{ url()->current() }}">
-    <meta property="og:image" content="{{ asset('img/LogoBR.png') }}?v=2">
+    <meta property="og:image" content="{{ asset('img/LogoBR.png') }}?v=3">
 
     <!-- Cookiebot -->
     <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="f3db7ff2-7bcd-414b-b5ec-ff4acacb7bc2" type="text/javascript" async></script>
